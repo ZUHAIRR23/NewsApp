0db3cb6d640cb9d9744c4ec41d39c54ec14815e1
Abdurahman Zuhair <abdurrahman.zuh@gmail.com>
2024-02-28 15:02:24 +0700

    update

---
+++ resources/views/home/category/index.blade.php
@@ -0,0 +1,16 @@
+@extends('home.parent')
+
+@section('content')
+    <div class="row">
+        <div class="card p-4">
+            <h3>Category</h3>
+
+            <div class="d-flex justify-content-end">
+                <button class="btn btn-primary">
+                    <i class="bi bi-plus"></i>
+                    Create Category
+                </button>
+            </div>
+        </div>
+    </div>
+@endsection
--- resources/views/home/includes/sidebar.blade.php
+++ resources/views/home/includes/sidebar.blade.php
@@ -3,11 +3,17 @@
     <ul class="sidebar-nav" id="sidebar-nav">
 
         <li class="nav-item">
-            <a class="nav-link " href="index.html">
+            <a class="nav-link " href="{{ route('home') }}">
                 <i class="bi bi-grid"></i>
                 <span>Home</span>
             </a>
         </li><!-- End Dashboard Nav -->
+        <li class="nav-item">
+            <a class="nav-link mt-2" href="{{ route('category.index') }}">
+                <i class="bi bi-basket2"></i>
+                <span>Category</span>
+            </a>
+        </li>
 
         <!-- End Blank Page Nav -->
 
