b19e19f6ac4e23e93b861d390390672c879bae9f
Abdurahman Zuhair <abdurrahman.zuh@gmail.com>
2024-03-28 11:05:13 +0700

    update

--- app/Http/Controllers/API/NewsController.php
+++ app/Http/Controllers/API/NewsController.php
@@ -2,10 +2,12 @@
 
 namespace App\Http\Controllers\API;
 
+use App\Models\News;
+use Illuminate\Support\Str;
+use Illuminate\Http\Request;
 use App\Helpers\ResponseFormatter;
 use App\Http\Controllers\Controller;
-use App\Models\News;
-use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Storage;
 
 class NewsController extends Controller
 {
@@ -33,6 +35,117 @@ class NewsController extends Controller
             return ResponseFormatter::success(
                 $news,
                 'Data news by id'
+            );
+        } catch (\Exception $error) {
+            return ResponseFormatter::error([
+                'message' => 'Something went wrong',
+                'error' => $error,
+            ], 'Authentication Failed', 500);
+        }
+    }
+
+    public function store(Request $request)
+    {
+        try {
+            // validate
+            $this->validate($request, [
+                'title' => 'required',
+                'category_id' => 'required',
+                'image' => 'required|mimes:jpeg,png,jpg|max:5120',
+                'content' => 'required'
+            ]);
+
+            // upload image
+            $image = $request->file('image');
+            $image->storeAs('public/news', $image->hashName());
+
+            // create data
+            $news = News::create([
+                'title' => $request->title,
+                'slug' => Str::slug($request->title),
+                'category_id' => $request->category_id,
+                'content' => $request->content,
+                'image' => $image->hashName()
+            ]);
+
+            return ResponseFormatter::success([
+                $news,
+                'Create Data Berhasil Ditambahkan'
+            ]);
+        } catch (\Exception $error) {
+            return ResponseFormatter::error([
+                'message' => 'Something went wrong',
+                'error' => $error,
+            ], 'Authentication Failed', 500);
+        }
+    }
+
+    public function destroy($id)
+    {
+        try {
+            // get data by id
+            $news = News::findOrFail($id);
+
+            // delete image
+            Storage::disk('local')->delete('/public/news/' . basename($news->image));
+
+            // delete data
+            $news->delete();
+
+            return ResponseFormatter::success(
+                null,
+                'Data Category Berhasil Dihapus'
+            );
+        } catch (\Exception $error) {
+            return ResponseFormatter::error([
+                'message' => 'Something went wrong',
+                'error' => $error,
+            ], 'Authentication Failed', 500);
+        }
+    }
+
+    public function update(Request $request, $id)
+    {
+        try {
+            // validate
+            $this->validate($request, [
+                'title' => 'required',
+                'category_id' => 'required',
+                'image' => 'image|mimes:jpeg,png,jpg|max:5120',
+                'content' => 'required'
+            ]);
+
+            // get data by id
+            $news = News::findOrFail($id);
+
+            if ($request->file('image') == '') {
+                $news->update([
+                    'title' => $request->title,
+                    'slug' => Str::slug($request->title),
+                    'category_id' => $request->category_id,
+                    'content' => $request->content
+                ]);
+            } else {
+                // delete old image
+                Storage::disk('local')->delete('/public/news' . basename($news->image));
+
+                // upload new image
+                $image = $request->file('image');
+                $image->storeAs('public/news', $image->hashName());
+
+                // update data
+                $news->update([
+                    'title' => $request->title,
+                    'slug' => Str::slug($request->title),
+                    'category_id' => $request->category_id,
+                    'content' => $request->content,
+                    'image' => $image->hashName()
+                ]);
+            }
+
+            return ResponseFormatter::success(
+                $news,
+                'Data News Has Been UpdatedP'
             );
         } catch (\Exception $error) {
             return ResponseFormatter::error([
--- routes/api.php
+++ routes/api.php
@@ -21,13 +21,21 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 Route::group(['middleware' => ['auth:sanctum']], function() {
     Route::post('/logout', [\App\Http\Controllers\API\AuthController::class, 'logout']);
     Route::post('/updatePassword', [\App\Http\Controllers\API\AuthController::class, 'updatePassword']);
+    Route::post('/storeProfile', [\App\Http\Controllers\API\AuthController::class, 'storeProfile']);
+    Route::post('/updateProfile', [\App\Http\Controllers\API\AuthController::class, 'updateProfile']);
 });
 
 // Route For Admin
 Route::group(['middleware'=> ['auth:sanctum', 'admin']], function() {
+    // Route Category
     Route::post('/category/create', [\App\Http\Controllers\API\CategoryController::class, 'store']);
     Route::post('/category/update/{id}', [\App\Http\Controllers\API\CategoryController::class, 'update']);
     Route::delete('/category/destroy/{id}', [\App\Http\Controllers\API\CategoryController::class, 'destroy']);
+
+    // Route News
+    Route::post('/news/create', [\App\Http\Controllers\API\NewsController::class, 'store']);
+    Route::delete('/news/destroy/{id}', [\App\Http\Controllers\API\NewsController::class, 'destroy']);
+    Route::post('/news/update/{id}', [\App\Http\Controllers\API\NewsController::class, 'update']);
 });
 
 Route::post('/login', [\App\Http\Controllers\API\AuthController::class, 'login']);
